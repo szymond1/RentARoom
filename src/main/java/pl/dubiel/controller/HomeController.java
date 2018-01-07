0c49fba4344f9e26cfff297848c5e9fbb6c697b3
dubiel.szymon@gmail.com <dubiel.szymon@gmail.com>
2018-01-07 01:52:59 +0100

    Configure pom.xml, AppConfig and AppInitializer

---
+++ src/main/java/pl/dubiel/controller/HomeController.java
@@ -0,0 +1,15 @@
+package pl.dubiel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/home")
+	@ResponseBody
+	public String home() {
+		return "home";
+	}
+}
